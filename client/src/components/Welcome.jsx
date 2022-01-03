d091b790e4677c5756cd1b7d50e661aff7635098
ian038 <ianphua3@gmail.com>
2022-01-03 11:03:51 -0500

    Initialized hardhat

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -39,7 +39,7 @@ export default function Welcome() {
                     Send Crypto <br /> across the world
                 </h1>
                 <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-                    Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
+                    Explore the crypto world. Buy and sell cryptocurrencies easily on Kryptik.
                 </p>
                 {/* {!currentAccount && (
                     <button
